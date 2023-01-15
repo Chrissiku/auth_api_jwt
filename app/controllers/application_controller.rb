74b12ee6289bbf3589a859b7f6f7a406f23a9f39
Chris Siku <chrissiku5@gmail.com>
2023-01-15 16:26:58 +0200

    Create authenticate_request function

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,14 @@
 class ApplicationController < ActionController::API
+    include JsonWebToken
+    
+    before_action :authenticate_request
+
+    private
+
+    def authenticate_request
+        header = request.headers['Authorization']
+        header = header.split(' ').last if header
+        decoded_token = jwt_decode(header)
+        @current_user = User.find(decoded_token[:user_id])
+    end
 end
